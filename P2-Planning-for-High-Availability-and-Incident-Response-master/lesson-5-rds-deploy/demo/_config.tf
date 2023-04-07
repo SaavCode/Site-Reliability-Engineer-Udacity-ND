d3219cdda6d41171849a54f710515c8d6b5c8a08
Nestor Saavedra <31100486+TheAstronauta@users.noreply.github.com>
2023-04-07 16:29:45 -0700

    Add files via upload

---
+++ P2-Planning-for-High-Availability-and-Incident-Response-master/lesson-5-rds-deploy/demo/_config.tf
@@ -0,0 +1,17 @@
+terraform {
+   backend "s3" {
+     bucket = "udacity-tf-tscotto"
+     key    = "terraform/terraform.tfstate"
+     region = "us-east-2"
+   }
+ }
+
+ provider "aws" {
+   region = "us-east-2"
+   
+ }
+
+ provider "aws" {
+  alias  = "usw1"
+  region = "us-west-1"
+}
