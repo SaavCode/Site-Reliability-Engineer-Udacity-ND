39666ec39d0f74bcefecbe9ebe4daa9811e12852
Nestor Saavedra <31100486+TheAstronauta@users.noreply.github.com>
2023-04-08 15:42:09 -0700

    Update main.tf

--- P2-Planning-for-High-Availability-and-Incident-Response-master/project/starter-code/zone1/main.tf
+++ P2-Planning-for-High-Availability-and-Incident-Response-master/project/starter-code/zone1/main.tf
@@ -15,7 +15,7 @@ locals {
 
    account_owner = local.name
    name          = "${local.name}-project"
-   azs           = ["us-east-2a","us-east-2b"]
+   azs           = ["us-east-2a","us-east-2b","us-east-2c"]
    private_subnet_tags = {
      "kubernetes.io/role/internal-elb" = 1
    }
@@ -30,7 +30,7 @@ locals {
 
    account_owner = local.name
    name          = "${local.name}-project"
-   azs           = ["us-west-1b","us-west-1c"]
+   azs           = ["us-west-1a","us-west-1c"]
    private_subnet_tags = {
      "kubernetes.io/role/internal-elb" = 1
    }
