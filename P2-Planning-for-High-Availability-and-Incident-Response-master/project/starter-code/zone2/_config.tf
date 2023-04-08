dc2653022d7364e68aa085cbbd16f6ad44650e1b
Nestor Saavedra <31100486+TheAstronauta@users.noreply.github.com>
2023-04-08 14:48:30 -0700

    update config terraform

--- P2-Planning-for-High-Availability-and-Incident-Response-master/project/starter-code/zone2/_config.tf
+++ P2-Planning-for-High-Availability-and-Incident-Response-master/project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-travis-west"
+     bucket = "udacity-tf-nsaavedra-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
