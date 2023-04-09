03c65eb349cd8ff9f262688fdddaf4d6d878adeb
Nestor Saavedra <31100486+TheAstronauta@users.noreply.github.com>
2023-04-08 18:26:51 -0700

    update

--- P2-Planning-for-High-Availability-and-Incident-Response-master/project/starter-code/zone1/modules/rds-s/rds.tf
+++ P2-Planning-for-High-Availability-and-Incident-Response-master/project/starter-code/zone1/modules/rds-s/rds.tf
@@ -1,18 +1,19 @@
-variable primary_db_cluster_arn {}
+variable "primary_db_cluster_arn" {}
+variable "primary_db_instance_arn" {}
 
 resource "aws_rds_cluster_parameter_group" "cluster_pg-s" {
   name   = "udacity-pg-s"
   family = "aurora-mysql5.7"
 
   parameter {
-    name  = "binlog_format"    
-    value = "MIXED"
+    name         = "binlog_format"
+    value        = "MIXED"
     apply_method = "pending-reboot"
   }
 
   parameter {
-    name = "log_bin_trust_function_creators"
-    value = 1
+    name         = "log_bin_trust_function_creators"
+    value        = 1
     apply_method = "pending-reboot"
   }
 }
@@ -23,34 +24,37 @@ resource "aws_db_subnet_group" "udacity_db_subnet_group" {
 }
 
 resource "aws_rds_cluster" "udacity_cluster-s" {
-  cluster_identifier       = "udacity-db-cluster-s"
-  availability_zones       = ["us-west-1a", "us-west-1c"]
+  allow_major_version_upgrade     = true
+  apply_immediately               = true
+  cluster_identifier              = "udacity-db-cluster-s"
+  availability_zones              = ["us-west-1a", "us-west-1c"]
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg-s.name
-  database_name            = "udacityc2"
-  master_username          = "udacity"
-  master_password          = "MyUdacityPassword"
-  vpc_security_group_ids   = [aws_security_group.db_sg_2.id]
-  db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
-  engine_mode              = "provisioned"
-  engine                   = "aurora-mysql"
-  engine_version           = "5.7.mysql_aurora.2.11.2"
-  skip_final_snapshot      = true
-  storage_encrypted        = false
-  depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
+  vpc_security_group_ids          = [aws_security_group.db_sg_2.id]
+  db_subnet_group_name            = aws_db_subnet_group.udacity_db_subnet_group.name
+  engine_mode                     = "provisioned"
+  engine                          = "aurora-mysql"
+  engine_version                  = "5.7.mysql_aurora.2.11.2"
+  skip_final_snapshot             = true
+  storage_encrypted               = false
+  backup_retention_period         = 5
+  replication_source_identifier   = var.primary_db_cluster_arn
+  source_region                   = "us-east-2"
+  depends_on                      = [var.primary_db_instance_arn]
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance-s" {
-  count                = 1
+  count                = var.rds_instance_count
   identifier           = "udacity-db-instance-${count.index}-s"
   cluster_identifier   = aws_rds_cluster.udacity_cluster-s.id
   instance_class       = "db.t2.small"
   engine               = "aurora-mysql"
+  engine_version       = "5.7.mysql_aurora.2.11.2"
   db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
 }
 
 resource "aws_security_group" "db_sg_2" {
   name   = "udacity-db-sg"
-  vpc_id =  var.vpc_id
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 3306
